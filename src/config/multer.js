c7f4044ce65a67d011343b6dec7a99ab7581ff51
Gabriel S. Gama <77582397+gabrielbrabo@users.noreply.github.com>
2023-02-15 21:32:36 -0300

    adição de função que faz upload de arquivos para bucket da aws s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,9 +1,13 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+
+//const GOOGLE_API_FOLDER-ID = '1VFmGYXxJeACE0GMl6mjwMaNH6Fm8bZmc'
 
 const storageTypes = {
-    local: multer.diskStorage({
+    local: multer.diskStorage({ 
         destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
         },
@@ -17,11 +21,27 @@ const storageTypes = {
             });
         }
     }),
+
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: process.env.BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: "public-read",
+        key: (req, file, cb) => {
+          crypto.randomBytes(16, (err, hash) => {
+            if (err) cb(err);
+    
+            const fileName = `${hash.toString("hex")}-${file.originalname}`;
+    
+            cb(null, fileName);
+          });
+        }
+    })
 }
 
 module.exports = {
     dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-    storage: storageTypes['local'],
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: 2 * 1024 * 1024
     },
--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,12 @@ const routes = express.Router()
 routes.post('/register', UsersController.create)
 routes.post('/sessions', SessionsController.create)
 
-
+routes.get("/post", async (req, res) => {
+    const posts = await PostFile.find();
+  
+    return res.json(posts);
+});
+  
 
 routes.post('/post', multer(multerConfig).single("file"),  async (req, res) => {
     const { originalname: name, size, key, location: url = "" } = req.file;
@@ -25,6 +30,14 @@ routes.post('/post', multer(multerConfig).single("file"),  async (req, res) => {
 
     return res.json(post);
 })
+
+routes.delete("/post/:id", async (req, res) => {
+    const post = await PostFile.findById(req.params.id);
+  
+    await post.remove();
+  
+    return res.send();
+});
 
 
 module.exports = routes
